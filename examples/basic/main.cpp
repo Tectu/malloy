3eee993939338c5bc75b28f36d350763306171d5
Joel Bodenmann <joel.bodenmann@simulton.com>
2021-03-30 17:40:50 +0200

    Simplify basic example

--- examples/basic/main.cpp
+++ examples/basic/main.cpp
@@ -2,19 +2,12 @@
 #include "../../lib/malloy/http/router.hpp"
 #include "../common/logger.hpp"
 
-#include <boost/asio/signal_set.hpp>
-#include <spdlog/logger.h>
-
 #include <cstdlib>
-#include <functional>
 #include <iostream>
 #include <memory>
 #include <string>
 #include <thread>
 #include <vector>
-
-namespace net = boost::asio;
-using tcp = boost::asio::ip::tcp;
 
 int main(int argc, char* argv[])
 {
@@ -40,12 +33,12 @@ int main(int argc, char* argv[])
             "    malloy 0.0.0.0 8080 . 1\n";
         return EXIT_FAILURE;
     }
-    auto const address = net::ip::make_address(argv[1]);
+    auto const address = boost::asio::ip::make_address(argv[1]);
     auto const port = static_cast<unsigned short>(std::atoi(argv[2]));
     auto const doc_root = std::make_shared<std::filesystem::path>(argv[3]);
     auto const threads = std::max<int>(1, std::atoi(argv[4]));
 
-    // Create request handler
+    // Create the router
     auto router = std::make_shared<malloy::http::server::router>(logger->clone("router"));
     {
         using namespace malloy::http;
@@ -93,32 +86,19 @@ int main(int argc, char* argv[])
     }
 
     // The io_context is required for all I/O
-    net::io_context ioc{threads};
+    boost::asio::io_context ioc{threads};
 
     // Create and launch a listening port
     std::make_shared<malloy::server::listener>(
         logger->clone("listener"),
         ioc,
-        tcp::endpoint{address, port},
+        boost::asio::ip::tcp::endpoint{address, port},
         router,
         doc_root
     )->run();
 
-    // Capture SIGINT and SIGTERM to perform a clean shutdown
-    boost::asio::signal_set signals(ioc, SIGINT, SIGTERM);
-    signals.async_wait(
-        [&](boost::system::error_code const&, int)
-        {
-            // Stop the `io_context`. This will cause `run()`
-            // to return immediately, eventually destroying the
-            // `io_context` and all of the sockets in it.
-            ioc.stop();
-        }
-    );
-
-    logger->info("starting server...");
-
     // Run the I/O service on the requested number of threads
+    logger->info("starting server...");
     std::vector<std::thread> v;
     v.reserve(threads - 1);
     for(auto i = threads - 1; i > 0; --i)
@@ -128,16 +108,6 @@ int main(int argc, char* argv[])
                 ioc.run();
             });
     ioc.run();
-
-    // If we get here, it means that we got a SIGINT or SIGTERM
-    logger->info("received SIGINT or SIGTERM.");
-
-    logger->info("waiting for all i/o threads to finish...");
-    for (auto& t : v)
-        t.join();
-
-    // Shut down complete.
-    logger->info("shutdown complete.");
 
     return EXIT_SUCCESS;
 }
