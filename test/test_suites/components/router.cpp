9ed5be2f5133e3a0ca7b8f58994205e1092f8e37
Joel Bodenmann <joel@embedded.pro>
2021-04-27 12:47:43 +0200

    Unit tests: Add [components - router] test suite

---
+++ test/test_suites/components/router.cpp
@@ -0,0 +1,58 @@
+#include "../../test.hpp"
+
+#include <malloy/http/router.hpp>
+
+using namespace malloy::http;
+using namespace malloy::http::server;
+
+TEST_SUITE("components - router")
+{
+
+    TEST_CASE("add [redirect]")
+    {
+        router r;
+
+        SUBCASE("valid [permanent]")
+        {
+            CHECK(r.add_redirect(status::permanent_redirect, "/foo", "/bar"));
+        }
+
+        SUBCASE("valid [temporary]")
+        {
+            CHECK(r.add_redirect(status::temporary_redirect, "/foo", "/bar"));
+        }
+
+        SUBCASE("invalid [status code]")
+        {
+            CHECK_FALSE(r.add_redirect(status::ok, "/foo", "/bar"));
+        }
+
+        SUBCASE("invalid [resource old]")
+        {
+            SUBCASE("empty")
+            {
+                CHECK_FALSE(r.add_redirect(status::permanent_redirect, "/foo", "/bar"));
+            }
+
+            SUBCASE("no leading slash")
+            {
+                CHECK_FALSE(r.add_redirect(status::permanent_redirect, "foo", "/bar"));
+            }
+        }
+
+        SUBCASE("invalid [resource new]")
+        {
+            SUBCASE("empty")
+            {
+                CHECK_FALSE(r.add_redirect(status::permanent_redirect, "/foo", ""));
+            }
+
+            SUBCASE("no leading slash")
+            {
+                CHECK_FALSE(r.add_redirect(status::permanent_redirect, "/foo", "bar"));
+            }
+        }
+
+    }
+
+}
