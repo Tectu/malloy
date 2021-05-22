db1fcc23196851c70c90d2fb783b2067d1e0b532
Joel Bodenmann <joel.bodenmann@simulton.com>
2021-05-22 21:58:38 +0200

    Remove unnecessary include

--- lib/malloy/server/routing/router.hpp
+++ lib/malloy/server/routing/router.hpp
@@ -9,7 +9,6 @@
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
 #include <boost/beast/version.hpp>
-#include <boost/beast/websocket/impl/rfc6455.hpp>
 
 #include <spdlog/logger.h>
 
