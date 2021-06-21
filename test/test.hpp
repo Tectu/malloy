1f9627c0e0ea0774f02db65c45a9e0bba2cfe208
Joel Bodenmann <joel@embedded.pro>
2021-06-21 21:41:31 +0200

    Add include to test.hpp Ref #8

--- test/test.hpp
+++ test/test.hpp
@@ -1,3 +1,12 @@
 #pragma once
 
 #include "doctest.hpp"
+
+/*
+ * These includes get included by doctest.hpp. However, there is a note:
+ *
+ * "required includes - will go only in one translation unit!"
+ *
+ * which means that we have to include them again as we generate multiple translation units.
+ */
+#include <iostream>
