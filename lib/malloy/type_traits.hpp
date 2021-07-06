059a2e7b31a032cdde3838b65187171ed785475e
0x00002a <ashenglandelbro@protonmail.com>
2021-07-06 11:23:05 +0100

    chore: format

--- lib/malloy/type_traits.hpp
+++ lib/malloy/type_traits.hpp
@@ -1,39 +1,42 @@
 #pragma once
 
-#include <boost/beast/core/buffer_traits.hpp>
 #include <boost/asio/buffer.hpp>
+#include <boost/beast/core/buffer_traits.hpp>
 
 #include "malloy/error.hpp"
 
 #include <concepts>
 #include <variant>
 
-namespace malloy::concepts {
+namespace malloy::concepts
+{
 
-template<typename B>
-concept const_buffer_sequence = boost::asio::is_const_buffer_sequence<B>::value;
+    template<typename B>
+    concept const_buffer_sequence = boost::asio::is_const_buffer_sequence<B>::value;
 
-template<typename Func>
-concept accept_handler = std::invocable<Func, malloy::error_code>;
+    template<typename Func>
+    concept accept_handler = std::invocable<Func, malloy::error_code>;
 
-template<typename B>
-concept dynamic_buffer = boost::asio::is_dynamic_buffer<B>::value;
+    template<typename B>
+    concept dynamic_buffer = boost::asio::is_dynamic_buffer<B>::value;
 
-template<typename Func>
-concept async_read_handler = std::invocable<Func, boost::beast::error_code, std::size_t>;
+    template<typename Func>
+    concept async_read_handler = std::invocable<Func, boost::beast::error_code, std::size_t>;
 
-template<typename V>
-concept is_variant = requires(V v) { 
-    []<typename... Args>(const std::variant<Args...>& vs){}(v); // https://stackoverflow.com/q/68115853/12448530
-};
+    template<typename V>
+    concept is_variant = requires(V v)
+    {
+        []<typename... Args>(const std::variant<Args...>& vs){}(v);    // https://stackoverflow.com/q/68115853/12448530
+    };
 
 
-template<typename V, template<typename> typename T>
-concept is_variant_of = requires(const V& v) {
-    [] <typename... Args>(const std::variant<T<Args>...>&) {}(v);
-};
+    template<typename V, template<typename> typename T>
+    concept is_variant_of = requires(const V& v)
+    {
+        []<typename... Args>(const std::variant<T<Args>...>&){}(v);
+    };
 
-}
+}    // namespace malloy::concepts
 
 /** 
  * @page general_concepts Core Concepts
@@ -55,5 +58,3 @@ concept is_variant_of = requires(const V& v) {
  *
  *
  */
-
-
