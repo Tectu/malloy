a104278451f10fda08979d8b07d61faef4f04444
Ash England-Elbro <46329225+0x00002a@users.noreply.github.com>
2021-07-14 16:18:43 +0100

    [Enhancement] Add option to build as shared libraries (#67)
    Closes: #55
    
    * feat: add option to build shared
    
    * chore: make everything output into bin dir
    
    * chore: fix ci
    
    * chore: switch to MALLOY_LIBRARY_TYPE rather than malloy_add_library
    
    * chore: fix mingw ci

--- examples/server/example.cmake
+++ examples/server/example.cmake
@@ -4,4 +4,6 @@ function(malloy_example_setup target)
         PRIVATE
             malloy-server
     )
+
+    set_target_properties(${TARGET} PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${MALLOY_BINARY_DIR})
 endfunction()
