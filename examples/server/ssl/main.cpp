bf84707456ccedc06b735bcccbe8a6b8fdac2915
Joel Bodenmann <joel.bodenmann@simulton.com>
2021-07-15 19:56:41 +0200

    Fix malloy-examples-server-ssl with TLS enabled

--- examples/server/ssl/main.cpp
+++ examples/server/ssl/main.cpp
@@ -24,8 +24,8 @@ int main()
     }
 
     // Setup TLS (SSL)
-    const auto& cert_path = doc_root / "malloy.cert";
-    const auto& key_path  = doc_root / "malloy.key";
+    const auto& cert_path = examples_doc_root / "malloy.cert";
+    const auto& key_path  = examples_doc_root / "malloy.key";
     if (!c.init_tls(cert_path, key_path)) {
         std::cerr<< "could not initialize TLS context." << std::endl;
         return EXIT_FAILURE;
