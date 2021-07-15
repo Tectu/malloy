8cad1c571657ff96a1797029451b9269fe99e286
Natasha England-Elbro <ashenglandelbro@protonmail.com>
2021-07-15 16:33:13 +0100

    chore: added test to repro issue (somewhat accurately)

--- test/test_suites/components/websockets.cpp
+++ test/test_suites/components/websockets.cpp
@@ -151,6 +151,42 @@ TEST_SUITE("websockets")
 {
     constexpr uint16_t port = 13312;
 
+    TEST_CASE("interleaved send/reads") {
+        constexpr uint16_t local_port = 13313;
+        constexpr auto bounceback = "Hello";
+        ws_roundtrip(local_port, [](auto& c_ctrl){
+            c_ctrl.ws_connect("127.0.0.1", local_port, "/", [](auto ec, auto conn){
+                    REQUIRE(!ec);
+                    auto read_buff = std::make_shared<boost::beast::flat_buffer>();
+                    conn->read(*read_buff, [conn](auto ec, auto){
+                        REQUIRE(!ec);
+                        auto msg = std::make_shared<std::string>(bounceback);
+                        conn->send(malloy::buffer(msg->data(), msg->size()), [](auto ec, auto){
+                            REQUIRE(!ec);
+                        });
+                    });
+            });
+        }, [](auto& s_ctrl){
+                s_ctrl.router()->add_websocket("/", [](const auto& req, auto conn){
+                    conn->accept(req, [conn]{
+                        auto msg = std::make_shared<std::string>(bounceback);
+                        conn->send(malloy::buffer(msg->data(), msg->size()), [msg](auto ec, auto){
+                            CHECK(!ec);
+                        });
+                        conn->send(malloy::buffer(msg->data(), msg->size()), [msg](auto ec, auto){
+                            CHECK(!ec);
+                        });
+                        auto read_buff = std::make_shared<boost::beast::flat_buffer>();
+                        conn->read(*read_buff, [msg, read_buff](auto ec, auto){
+                            REQUIRE(!ec);
+                            const auto data = boost::beast::buffers_to_string(read_buff->data());
+                            CHECK(data == *msg);
+                        });
+                    });
+                });
+            });
+    }
+
     TEST_CASE("roundtrips")
 	{
 	    SUBCASE("plain, text_mode")
