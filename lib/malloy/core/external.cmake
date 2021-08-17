8ef8de5433a83a850966b0c774243d7c798b8eef
Ash England-Elbro <ashenglandelbro@protonmail.com>
2021-08-17 12:41:20 +0100

    chore: bump spdlog to 1.9.2

--- lib/malloy/core/external.cmake
+++ lib/malloy/core/external.cmake
@@ -41,7 +41,7 @@ if (MALLOY_DEPENDENCY_SPDLOG_DOWNLOAD)
     FetchContent_Declare(
         spdlog
         GIT_REPOSITORY https://github.com/gabime/spdlog
-        GIT_TAG        v1.9.0
+        GIT_TAG        v1.9.2
     )
     FetchContent_GetProperties(spdlog)
     if (NOT spdlog_POPULATED) 
