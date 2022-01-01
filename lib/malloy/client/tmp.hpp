a4c84be2d8c94610167d7f2471429322e00a845e
Ash England-Elbro <ashenglandelbro@protonmail.com>
2022-01-01 14:44:07 +0000

    chore: add docs

--- lib/malloy/client/tmp.hpp
+++ lib/malloy/client/tmp.hpp
@@ -7,6 +7,7 @@
 namespace malloy::client::tmp {
 
 namespace detail {
+/// Helper to map variant<T...> to variant<response<T>...>
 template<template<typename...> typename V, typename... Vargs>
 struct conv_to_resp_helper {
     constexpr conv_to_resp_helper(V<Vargs...>) {}
@@ -14,17 +15,23 @@ struct conv_to_resp_helper {
 };
 }
 
+/**
+ * @brief Converts from a variant of possible bodies to the actual body type taken by callbacks
+ */
 template<typename V>
 requires (malloy::concepts::is_variant_of_bodies<V>)
 using body_type = malloy::tmp::unwrap_variant<V>;
 
+/// Resolves the body type used by a filter F
 template<typename F>
 using bodies_for_t = std::invoke_result_t<decltype(&F::body_for), const F*, const typename F::header_type&>;
 
+/// Converts a variant<T...> where T is body to a variant<response<T>...>
 template<typename Bodies>
 requires (malloy::concepts::is_variant_of_bodies<Bodies>)
 using to_responses = typename decltype(detail::conv_to_resp_helper{std::declval<Bodies>()})::type;
 
+/// Resolves to the type that must be taken in callbacks handling responses for Filter
 template<typename Filter>
 using filter_resp_t = malloy::tmp::unwrap_variant<to_responses<bodies_for_t<Filter>>>;
 
--- lib/malloy/core/tmp.hpp
+++ lib/malloy/core/tmp.hpp
@@ -5,6 +5,21 @@
 #include <variant>
 
 namespace malloy::tmp {
+
+/**
+ * @brief Pattern:
+ *  unwrap_variant<variant<T>> -> T
+ *  unwrap_variant<variant<T, ...> -> std::variant<T, ...>
+ *
+ *  i.e. If its a 1 element it just returns that type, otherwise the entire variant type
+ */
 template<typename V>
 using unwrap_variant = std::conditional_t<std::variant_size_v<V> == 1, std::variant_alternative_t<0, V>, V>;
 }
+
+/**
+ * @namespace malloy::tmp
+ * @brief Contains template metaprogramming helpers
+ *
+ */
+
