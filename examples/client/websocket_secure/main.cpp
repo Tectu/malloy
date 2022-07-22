6a03230fc47ea21901acf95c8a8ea17d4ad7c313
Joel Bodenmann <jbo@insane.engineer>
2022-07-22 12:23:43 +0000

    examples: improve client/websocket_secure

--- examples/client/websocket_secure/main.cpp
+++ examples/client/websocket_secure/main.cpp
@@ -15,53 +15,74 @@ int main()
     // Create the controller
     malloy::client::controller c{cfg};
 
-    // Start
+    // Initialize TLS
     if (!c.init_tls()) {
         std::cerr << "initializing TLS context failed." << std::endl;
         return EXIT_FAILURE;
     }
-    c.add_ca_file("../../../../examples/server/static_content/malloy.cert");
+    c.add_ca_file("../../examples/server/static_content/malloy.cert");
 
-    auto session = start(c);
+    // Start
+    [[maybe_unused]] auto session = start(c);
 
+    // Connect to the /echo endpoint of the websocket example server
     c.wss_connect(
         "127.0.0.1",
         8080,
         "/echo",
         [](malloy::error_code ec, auto conn) {
+            // Was the connection attempt successful?
             if (ec) {
-                std::cerr << "Couldn't connect: " << ec.message() << '\n';
+                std::cerr << "could not connect: " << ec.message() << '\n';
                 return;
             }
+
             conn->send(malloy::buffer("Hello from Malloy!"), [conn](auto ec, auto) {
+                // Was the sending attempt successful?
                 if (ec) {
-                    std::cerr << "Uh oh, we couldn't send something: " << ec.message();
+                    std::cerr << "could not send message to server: " << ec.message() << "\n";
                     return;
                 }
-                malloy::examples::ws::oneshot_read(conn, [](malloy::error_code ec, std::string msg) {
-                    std::cout << "id[0]: " << msg << '\n';
-                    });
-                });
-        });
 
+                // Read
+                malloy::examples::ws::oneshot_read(conn, [](malloy::error_code ec, std::string msg) {
+                    std::cout << msg << '\n';
+                });
+            });
+    });
+
+    // Connect to the /timer endpoint of the websocket example server
     c.wss_connect(
         "127.0.0.1",
         8080,
         "/timer",
         [](malloy::error_code ec, auto conn) {
+            // Was the connection attempt successful?
             if (ec) {
-                std::cerr << "Uh oh, we have a problem: " << ec.message() << '\n';
+                std::cerr << "could not connect: " << ec.message() << '\n';
                 return;
             }
+
+            // Send something to the server
             conn->send(malloy::buffer("Whoop Whoop"), [conn](auto ec, auto) {
+                // Was the sending attempt successful?
+                if (ec) {
+                    std::cerr << "could not send message to server: " << ec.message() << "\n";
+                    return;
+                }
+
+                // Read
                 malloy::examples::ws::oneshot_read(conn, [](malloy::error_code ec, std::string msg) {
-                    std::cout << "id[1]: " << msg << std::endl;
-                    });
+                    std::cout << msg << std::endl;
                 });
+            });
         }
     );
 
-    session.run();
+    using namespace std::chrono_literals;
+    std::this_thread::sleep_for(15s);
+
+    // Session will stop the client controller when it goes out of scope
 
     return EXIT_SUCCESS;
 }
