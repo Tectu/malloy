2c22ad068756d406c6c265720af1619bfd53bbb1
Joel Bodenmann <jbo@insane.engineer>
2023-08-05 18:56:37 +0200

    http: add additional malloy::to_string() overload

--- lib/malloy/core/http/http.hpp
+++ lib/malloy/core/http/http.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "types.hpp"
+
 /**
  * @namespace malloy::http
  *
@@ -7,4 +9,24 @@
  */
 namespace malloy::http
 {
+}
+
+
+namespace malloy
+{
+
+    /**
+     * Convert HTTP method/verb to string.
+     *
+     * @param method The method.
+     * @return String representation.
+     */
+    [[nodiscard]]
+    inline
+    std::string
+    to_string(const malloy::http::method method)
+    {
+        return boost::beast::http::to_string(method);
+    }
+
 }
