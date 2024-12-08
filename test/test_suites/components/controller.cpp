6c0c029a50d8f51dcc605e186bd0be4959ddb347
Joel Bodenmann <jbo@insane.engineer>
2024-12-09 00:43:44 +0100

    Whitespaces

--- test/test_suites/components/controller.cpp
+++ test/test_suites/components/controller.cpp
@@ -23,12 +23,17 @@ static_assert(check_start_accessible<std::void_t<>>);
 static_assert(check_start_result<mc::controller&>, "start returns the type of start_result for client controller");
 static_assert(check_start_result<ms::routing_context>, "start returns the type of start_result for server controller");
 
-TEST_SUITE("controller - compile checks") {
-    TEST_CASE("start(...) is accessible directly from the parent namespace of controller/routing context"){
+TEST_SUITE("controller - compile checks")
+{
+    TEST_CASE("start(...) is accessible directly from the parent namespace of controller/routing context")
+    {
     }
 }
-TEST_SUITE("controller - roundtrips") {
-    TEST_CASE("A controller_run_result<T> where T is moveable is also movable and well defined") {
+
+TEST_SUITE("controller - roundtrips")
+{
+    TEST_CASE("A controller_run_result<T> where T is moveable is also movable and well defined")
+    {
         mc::controller::config cfg;
         cfg.logger = spdlog::default_logger();
         cfg.num_threads = 1;
@@ -36,19 +41,24 @@ TEST_SUITE("controller - roundtrips") {
 
         auto tkn = start(ctrl);
         auto tkn2 = std::move(tkn);
-        SUBCASE("calling run on a moved-from run result raises an exception") {
+
+        SUBCASE("calling run on a moved-from run result raises an exception")
+        {
             CHECK_THROWS(tkn.run());
         }
-        SUBCASE("calling run on the move constructed result is well defined") {
+
+        SUBCASE("calling run on the move constructed result is well defined")
+        {
             tkn2.run();
         }
     }
-    TEST_CASE("Server and client set agent strings based on user_agent") {
+
+    TEST_CASE("Server and client set agent strings based on user_agent")
+    {
         constexpr auto cli_agent_str = "test-cli";
         constexpr auto serve_agent_str = "test-serve";
         constexpr auto addr = "127.0.0.1";
         constexpr uint16_t port = 55123;
-
 
         malloy::controller::config general_cfg;
         general_cfg.logger = spdlog::default_logger();
@@ -75,7 +85,6 @@ TEST_SUITE("controller - roundtrips") {
 
         ms::routing_context serve_ctrl{serve_cfg};
 
-
         serve_ctrl.router().add(malloy::http::method::get, "/", [&](auto&& req){
             CHECK(req[malloy::http::field::user_agent] == cli_agent_str);
             return malloy::http::generator::ok();
@@ -85,7 +94,5 @@ TEST_SUITE("controller - roundtrips") {
         start(cli_ctrl).run();
 
         CHECK(!stop_tkn.get());
-
     }
 }
-
